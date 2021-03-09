5fa103687900475e86a26a1e10966708aedebf04
pulkit jain <pulkitjain708@gmail.com>
2021-03-10 01:32:33 +0530

    Restructuring Code

--- php/login.php
+++ php/login.php
@@ -6,14 +6,14 @@
     $login_query='select type from users where email= "'.$email.'" and password="'.$password.'";';
     if($result=$conn->query($login_query)){
         if($result->num_rows==0){
-        echo "<script>alert('No such users found');window.location.href='index.html';</script>";
+        echo "<script>alert('No such users found');window.history.back();</script>";
                                 }
         else{
         $user_type=$result->fetch_assoc()['type'];
         switch($user_type){
             case 0: echo 'WELCOME DEP HEAD'; break;
-            case 1 : echo "<script>window.location.href='teacher.html';</script>"; break;
-            case 2 : echo "<script>window.location.href='student.html';</script>"; break;
+            case 1 : echo "<script>location.href='/noticeboard/teacher/teacher.html';</script>"; break;
+            case 2 : echo "<script>location.href='/noticeboard/student/student.html';</script>"; break;
         }
         $result->free_result();
             }
