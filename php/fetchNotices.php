24d6043d46c45d593be0db0358708309bd08ac55
pulkit jain <pulkitjain708@gmail.com>
2021-04-17 23:10:30 +0530

    Added Pagination

--- php/fetchNotices.php
+++ php/fetchNotices.php
@@ -8,6 +8,7 @@ $from = $_POST['from'];
 $to=$_POST['to'];
 $expiry=$_POST['expiry'];
 $where='';
+$page=$_POST['page'];
 $first='select * from notifs where ';
 if(isset($for) && !empty($for))
 $where=$where.'forName = "'.$for.'"';
@@ -21,10 +22,22 @@ if(isset($from) && isset($to) && !empty($from) && !empty($to))
 $where=$where.' and expiry between "'.$from.'" and "'.$to.'"';
 else
 $where=$where.' and expiry = "'.$expiry.'"';
-$final=$first.$where.';';
+$final=$first.$where;
 // echo $final;
+$show_page=3;
+$count_query='SELECT count(*) from notifs';
+$result_count = $conn->query($count_query);
+$total_page = $result_count->fetch_assoc()['count(*)'];
+$divided_pages = ceil($total_page / $show_page);
+$first_display = ($page - 1) * $show_page;
+$final=$final . ' limit ' . $first_display . ', ' . $show_page . ";"; 
+// FINAL QUERY
 $result = mysqli_query($conn, $final);
 $json;
+if($page>$divided_pages){
+  echo json_encode(array("message"=>"Limit Exceeded"));  
+}
+else{
 if (mysqli_num_rows($result) > 0) {
   while($row = mysqli_fetch_assoc($result)) {
     $json[]= $row; 
@@ -34,6 +47,6 @@ if (mysqli_num_rows($result) > 0) {
   echo json_encode(0);
 }
 // echo $final;
-
+}
 mysqli_close($conn);
 ?>
